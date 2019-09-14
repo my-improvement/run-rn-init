26ea605ed9521c50af2e056b5d461d9f34fd72ac
reynaldpn <reynaldpn@gmail.com>
2019-09-14 14:48:50 +0700

    fix readme

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let     availableReactNativeVersions = []
 /* ----------------------------------------------------------------------------------------------- */
 
 commander
-    .version('0.2.2')
+    .version('0.2.3')
     .description('run-rn-init')
 
 commander
