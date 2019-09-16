bb6ca8383e4832d2489226c49fd556d7ef0d5ab5
reynaldpn <reynaldpn@gmail.com>
2019-09-16 10:27:01 +0700

    ignore screenshot files on npmignore

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let     availableReactNativeVersions = []
 /* ----------------------------------------------------------------------------------------------- */
 
 commander
-    .version('0.2.3')
+    .version('0.2.4')
     .description('run-rn-init')
 
 commander
